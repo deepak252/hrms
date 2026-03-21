ea231ccd653c08ce7bb5755a76a5269f633dee33
deepak252 <deepakkudc16102000@gmail.com>
2026-03-21 10:36:39 +0530

    bug fix

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,11 +8,11 @@ import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-  // </StrictMode>
+  <StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
 );
